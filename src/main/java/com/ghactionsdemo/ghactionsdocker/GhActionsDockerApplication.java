2069f5950ef15a0db1f6d18c40094bd3c951fc29
ShailyGarg <shaily93@gmail.com>
2021-12-25 23:25:57 +0530

    Add Spring Boot

---
+++ src/main/java/com/ghactionsdemo/ghactionsdocker/GhActionsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ghactionsdemo.ghactionsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhActionsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhActionsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghactionsdemo/ghactionsdocker/GhActionsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghactionsdemo.ghactionsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhActionsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
